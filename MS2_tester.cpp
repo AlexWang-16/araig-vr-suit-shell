954e8db8d733e40247a96d9cdae3360dfd36c585
Alex Wang <alex.lik.wang@gmail.com>
2016-11-27 12:02:24 -0500

    Milestone 2 completed.

--- MS2_tester.cpp
+++ MS2_tester.cpp
@@ -40,6 +40,21 @@ int main(int argc, const char * argv[]) {
   //Test: Creating a generic task and add 3 stimulations to the Task, then print out task information and details of each stimulation
   //Expected: It should print "Task 5 Task_5" followed by the stimulation information
   //TODO
+  Stims s1("Stim1", "back", 50, 50, 50);
+  Stims s2("Stims2", "front", 60, 60, 60);
+  Exoskeleton ex1 ("Exo1", 50, 100);
+  Task echo;
+  echo += s1.clone();
+  echo += s2.clone();
+  echo += ex1.clone();
+  echo.dump(std::cout);
+  echo.execute(std::cout);
+  std::cout << "\n\n";
+  
+  //Test: Print the 2nd element from Echo using overloaded [] operator
+  //Expected: Stims 2 data
+  echo[1]->display(std::cout);
+  
   
   //Test: Copy Task 5 using "=" operator
   //Expected: Information printed should be the same as Task 5 (see above)
